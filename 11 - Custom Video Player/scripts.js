eef7319c6cf3232f1f55d7aa7da0bfd5f7d0e1d4
GorynychArt <gorynychart@gmail.com>
2023-08-04 21:50:01 +0300

    Day 11 - Done

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -22,8 +22,43 @@ function updateButton() {
   toggle.textContent = this.paused ? '►' : '❚ ❚';
 }
 
+const skip = function () {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+};
+
+const handleRangeUpdate = function () {
+  video[this.name] = this.value;
+};
+
+const handleProgress = function () {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+};
+
+const scrub = function (e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+};
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => {
+  button.addEventListener('click', skip);
+});
+ranges.forEach(range => {
+  range.addEventListener('change', handleRangeUpdate);
+});
+ranges.forEach(range => {
+  range.addEventListener('mousemove', handleRangeUpdate);
+});
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
